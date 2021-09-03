6292ddf10366f46a41f9fb12ac1396d3dd92bb1e
Hwichan Jeon <gnlcks82@gmail.com>
2021-09-03 05:34:33 +0000

    solved

--- sickpickel/Week5/3109-sickpickel.cpp
+++ sickpickel/Week5/3109-sickpickel.cpp
@@ -2,38 +2,34 @@
 
 using namespace std;
 
-vector<string> street;
+int R, C;
+int dy[3] = { -1, 0, 1 };
+int dx[3] = { 1, 1, 1 };
+char street[10000][501];
+
+int dfs(int nowI, int nowJ){
+	street[nowI][nowJ] = 'x';
+	if(nowJ == C - 1){
+		return 1;
+	}
+	for(int i = 0; i < 3; i++){
+		int nextI = nowI + dy[i];
+		int nextJ = nowJ + dx[i];
+		if(nextI < 0 || nextI >= R) continue;
+		if(street[nextI][nextJ] == 'x') continue;
+		if(dfs(nextI, nextJ)) return 1;
+	}
+	return 0;
+}
 
 int main(void){
-	int R, C;
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	
 	cin >> R >> C;
-	string input;
-	for(int i = 0; i < R; i++){
-		getline(cin, input);
-		street.push_back(input);
-	}
+	for(int i = 0; i < R; i++) cin >> street[i];
 	
 	int answer = 0;	
-	for(int i = 0; i < R; i++){
-		int nowI = i;
-		int nowJ = 0;
-		while(nowI < R && nowJ < C){
-			if(street[nowI][nowJ + 1] != 'x') {
-				street[nowI][nowJ + 1] = 'x';
-				nowJ++;
-				continue;
-			}	
-			if(nowI + 1 < R && street[nowI + 1][nowJ + 1] != 'x'){
-				street[nowI + 1][nowJ + 1] = 'x';
-				nowI++; nowJ++;
-				continue;
-			}
-			break;
-		}	
-		if(nowJ == C - 1){
-			answer++;
-		}
-	}
+	for(int i = 0; i < R; i++) answer += dfs(i, 0);
 	cout << answer << "\n";	
 	return 0;
 }
