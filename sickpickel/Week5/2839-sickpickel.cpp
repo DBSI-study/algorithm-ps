21dc3d30a322917bbb2ca5b732334b26a80323c3
ksh0414 <89390688+ksh0414@users.noreply.github.com>
2021-09-03 14:17:25 +0900

    Revert "Revert "Merge branch 'main' of https://github.com/DBSI-study/algorithm-ps into main""
    This reverts commit 453a1003e8058ec50dc4a07c1af3f4b4dfe96c8e.

---
+++ sickpickel/Week5/2839-sickpickel.cpp
@@ -0,0 +1,14 @@
+// 설탕배달
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+	int N; cin >> N;
+	for(int cnt5kg = N / 5; cnt5kg >= 0; cnt5kg--){
+		int left = N - cnt5kg * 5;
+		if(left % 3 == 0) {cout << cnt5kg + left / 3 << "\n"; return 0;}
+	}
+	cout << "-1\n";	
+	return 0;
+}
