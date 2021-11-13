93e2043d1f826d61f7d5c836782b411459b3bd4f
wjsgnlcks20 <gnlcks82@gmail.com>
2021-11-13 20:14:38 +0900

    2505 yet to be solved

--- sickpickel/S2Week2/2505-sickpickel_debug.cpp
+++ sickpickel/S2Week2/2505-sickpickel_debug.cpp
@@ -30,11 +30,26 @@ void rotate(int begin, int end, int * toRotate){
 	}
 }
 
+int groupNum = 0;
+int arrPrintNum = 0;
+
 int isSorted(int * arr, int size){
+	int flag = 1;
+	cout << "Arr #" << arrPrintNum++ << " : ";
 	for(int i = 0; i < size; i++){
-		if(arr[i] != i + 1) return 0;
+		cout << arr[i] << " ";
+		if(arr[i] != i + 1) flag = 0;
 	}
-	return 1;
+	cout << "\n";
+	return flag;
+}
+
+void printArr(int * arr, int size){
+	cout << "Arr #" << arrPrintNum++ << " : ";
+	for(int i = 0; i < size; i++){
+		cout << arr[i] << " ";
+	}	
+	cout << "\n";
 }
 
 int solve(vector<pair<int, int>>& answer, int * arr, int N, int cnt){
@@ -43,18 +58,29 @@ int solve(vector<pair<int, int>>& answer, int * arr, int N, int cnt){
 	vector<pair<int, int>> group;
 
 	divideToGroup(group, arr, N);
+	cout << "Group #" << groupNum++ << " : ";
+	for(auto elem: group){
+		cout << elem.first << " : " << elem.second << " / ";
+	}
+	cout << "\n";
 	// pickGroupsToRotate
 	for(int i = 0; i < group.size(); i++){
 		for(int j = 0; j < group.size() - i; j++){
+			cout << "Selected Group : " << j << " to " << j + i << "\n";
+			if(cnt == 0 && j == 0 && j + i == 1) cout << "here!!!!!!\n";
+			if(cnt == 1 && j == 1 && j + i == 1) cout << "2nd here!!!!\n";
 			int begin = group[j].first;
 			int end = group[j + i].second;
 			answer.push_back({begin, end});
 
 			copyArr(arr, copyarr, N);
 			rotate(begin, end, arr);
+			// printArr(arr, N);
 
-			if(isSorted(arr, N)) return 1;
-			if(cnt == 0) if(solve(answer, arr, N, cnt + 1)) return 1;
+			if(cnt == 0) {
+				if(solve(answer, arr, N, cnt + 1)) return 1;
+			}
+			else if(isSorted(arr, N)) return 1;
 
 			copyArr(copyarr, arr, N);
 			answer.pop_back();
@@ -70,11 +96,8 @@ int main(void){
 	for(int i = 0; i < N; i++){
 		cin >> arr[i];
 	}
-	if(isSorted(arr, N)) { cout << "1 1\n1 1\n"; return 0;}
 	vector<pair<int, int>> answer;
 	solve(answer, arr, N, 0);
-	
-	if(answer.size() == 1) answer.push_back({1, 1});
 	for(auto elem : answer){
 		cout << elem.first + 1 << " " << elem.second + 1 << "\n";
 	}
