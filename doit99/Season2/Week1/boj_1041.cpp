bf0ae3997a260319b31a515993c001e7ab16babb
hyobn <qls0786@naver.com>
2021-11-21 14:42:53 +0000

    greedy

--- doit99/Season2/Week1/boj_1041.cpp
+++ doit99/Season2/Week1/boj_1041.cpp
@@ -1,54 +1,37 @@
 #include <iostream>
 #include <algorithm>
-#include <math.h>
-using namespace std;
+using namespace std; 
 
-int main()
-{
-	long long n;
-	long long result=0;
-	long long arr[7];
-	long long minimum=0;
-	cin>>n;
-	for(int i=0;i<6;i++)
-		cin>>arr[i];
+int main() {
+	long long n,m=0,ans=0;
+	long long arr[6]; 
+	cin>>n; 
 	
-	// n=1일때 고려.
-	if(n==1){
-		sort(arr,arr+6);
-
-		for(int i=0;i<5;i++)
-			result+=arr[i];
-		cout<<result;
+	for (int i=0;i<6;i++) {
+		cin>>arr[i]; 
+		ans+=arr[i];
+		m=max(m,arr[i]); 
+	} 
+	
+	if (n==1){
+		cout<<ans-m; 
 		return 0;
 	}
-	// 3면 보이는 경우의 주사위
-	
-	minimum+=min(min(min(arr[1]+arr[2],arr[2]+arr[4]),arr[3]+arr[4]),arr[1]+arr[3]);
-	minimum+=min(arr[0],arr[5]);
-	result+=4*minimum;
-	
-	// 2면 보이는 경우의 주사위
-	int comp=-5;
-	minimum=101;
-	for(int i=0;i<3;i++){
-		for(int j=0;j<6;j++){
-			if(i-j==comp || i==j) continue;
-			long long tmp=abs(arr[i]+arr[j]);
-			if(tmp<minimum)
-				minimum=tmp;
-				
-		}
-		comp+=2;
-	}
-	
-	result+=(minimum)*((n-1)*4+(n-2)*4);
-	
-	
-	// 1면 보이는 경우의 주사위
-	sort(arr,arr+6);
-	result+=arr[0]*((n-2)*(n-1)*4+(n-2)*(n-2));
-	
-	cout<<result;
-	
+
+	ans=0; 
+	arr[0]=min(arr[0],arr[5]);
+	arr[1]=min(arr[1],arr[4]); 
+	arr[2]=min(arr[2],arr[3]);
+
+	sort(arr,arr+3); 
+
+	int sum1=arr[0];
+	int sum2=sum1+arr[1];
+	int sum3=sum2+arr[2];
+
+	ans+=sum3*4; 
+	ans+=sum2*(4*(n-2)+4*(n-1));
+	ans+=sum1*(4*(n-1)*(n-2)+(n-2)*(n-2));
+	cout<<ans;
+
 }
