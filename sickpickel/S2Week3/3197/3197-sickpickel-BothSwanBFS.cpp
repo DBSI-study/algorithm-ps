5d47a0be0792ddd815d1a60a9ed2897e1556eadd
Hwichan Jeon <gnlcks82@gmail.com>
2021-12-28 14:57:22 +0000

    solved 3197 and rearranged s2w3

--- sickpickel/S2Week3/3197/3197-sickpickel-BothSwanBFS.cpp
+++ sickpickel/S2Week3/3197/3197-sickpickel-BothSwanBFS.cpp
@@ -1,3 +1,4 @@
+// 두 백조 모두 BFS 이동하여 서로 찾기
 #include <bits/stdc++.h>
 #define Max 1501
 #define pii pair<int, int>
@@ -8,8 +9,7 @@ int R, C;
 int dy[4] = {1, -1, 0, 0};
 int dx[4] = {0, 0, 1, -1};
 char Lake[Max][Max];
-bool visitedWater[Max][Max];
-queue<pii> water, nextWater, swan, nextSwan;
+bool visitedWater[Max][Max], visitedSwan[Max][Max];
 
 int isOutOfRange(pii pos){
     if(pos.first < 0 || pos.first >= R || pos.second < 0 || pos.second >= C)
@@ -17,39 +17,47 @@ int isOutOfRange(pii pos){
     return 0;
 }
 
-int canMeet(char visited, char find){
-	swan = nextSwan;
+int canMeet(queue<pii>& nextSwan, char from, char find){
+	
+	queue<pii> swan = nextSwan;
 	while(!nextSwan.empty()) nextSwan.pop();
 
     while(!swan.empty()){
         pii now = swan.front();
         swan.pop();
-
+		
+		// 백조의 첫 BFS 초기위치에 대한 visited 처리
+		visitedSwan[now.first][now.second] = 1;	
+		// 어느 방향에서 오는 백조인지를 구분하기위한 저장 
+		Lake[now.first][now.second] = from;
+		
         for(int i = 0; i < 4; i++){
             pii next = {now.first + dy[i], now.second + dx[i]};
             if(isOutOfRange(next)) continue;
-            if(Lake[next.first][next.second] == visited) continue;
-            if(Lake[next.first][next.second] == find) 
-			{ while(!swan.empty()) swan.pop(); return 1; }
+            if(visitedSwan[next.first][next.second]) continue;
+            if(Lake[next.first][next.second] == find) return 1;
 			
-            if(Lake[next.first][next.second] == 'X') 
-                {nextSwan.push(next); continue;}
-
-            Lake[next.first][next.second] = visited;
-            swan.push(next);
+            if(Lake[next.first][next.second] == '.') swan.push(next);
+			else nextSwan.push(next); 
+			
+			visitedSwan[next.first][next.second] = 1;
         }
     }
     return 0;
 }
 
-void meltIce(){
+void meltIce(queue<pii>& nextWater){
 	
+	queue<pii> water = nextWater;	
 	water = nextWater;
 	while(!nextWater.empty()) nextWater.pop();
 	
 	while(!water.empty()){
 		pii now = water.front();
 		water.pop();
+		
+		// water의 첫 BFS 초기위치에 대한 visited 처리
+		visitedWater[now.first][now.second] = 1;
 		
 		if(Lake[now.first][now.second] == 'X'){
 			Lake[now.first][now.second] = '.';
@@ -76,25 +84,30 @@ void printLake(){
 
 int main(void){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	vector<pii> swan;
-
+	
+	queue<pii> nextWater, nextSwan[2];
+	
     cin >> R >> C;
     for(int i = 0; i < R; i++){
         for(int j = 0; j < C; j++){
             cin >> Lake[i][j];
-            if(Lake[i][j] == 'L') swan.push_back({i, j});
+            if(Lake[i][j] == 'L') {
+				if(nextSwan[0].empty()) nextSwan[0].push({i, j});
+				else nextSwan[1].push({i, j});
+			}
 			if(Lake[i][j] != 'X') nextWater.push({i, j});
         }
     }
-	if(R == 1 && C == 1) { cout << "0\n"; return 0; }
-    nextSwan.push(swan[0]);
-	Lake[swan[1].first][swan[1].second] = '^';
-	
+
     int answer = 0;
-	meltIce();
-    while(1){
-        if(canMeet('*', '^')) break;
-        meltIce();
+	meltIce(nextWater);
+	
+	// 한쪽에서 만나지 못한다면
+    while(!canMeet(nextSwan[0], '*', '^')){
+		// 반대쪽도 이동가능한 범위까지 이동
+        canMeet(nextSwan[1], '^', '*');
+		// 얼음 녹이기
+        meltIce(nextWater);
         answer++;
     }
 	// printLake();
